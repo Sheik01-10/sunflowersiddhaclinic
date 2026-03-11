c56d48e92d591a8100c82e3d917548ac811bc9ac
Sheik01-10 <sheikalmadeen@gmail.com>
2026-03-11 19:04:07 +0530

    update

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth, setPersistence, browserLocalPersistence  } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCl7IpsB3hh75LFwN-xCl9s0gQ3v0bLO3Y",
-  authDomain: "sunflowersiddhaclinic-998b7.firebaseapp.com",
-  projectId: "sunflowersiddhaclinic-998b7",
-  storageBucket: "sunflowersiddhaclinic-998b7.firebasestorage.app",
-  messagingSenderId: "198026952923",
-  appId: "1:198026952923:web:2b5e079e81f2b0f634ccfe"
+  apiKey: "AIzaSyDjnskfkh1iNMVeB7JfSsQTgsw0ax5cySc",
+  authDomain: "sunflower-siddha-clinic-304ba.firebaseapp.com",
+  projectId: "sunflower-siddha-clinic-304ba",
+  storageBucket: "sunflower-siddha-clinic-304ba.firebasestorage.app",
+  messagingSenderId: "840323077949",
+  appId: "1:840323077949:web:38649a37bb9a02c8801754"
 };
 
 const app = initializeApp(firebaseConfig);
